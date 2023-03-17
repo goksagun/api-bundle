d175ad3c5fcca815414e8d3746f77a19bd179e8c
Burak Bolat <burak.bolat@hprotravel.com>
2023-03-17 11:59:06 +0300

    Add http rest route short attributes

---
+++ src/Component/Routing/Annotation/Get.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Goksagun\ApiBundle\Component\Routing\Annotation;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[\Attribute]
+class Get extends Route
+{
+    public function getMethods(): array
+    {
+        return [
+            Request::METHOD_GET,
+        ];
+    }
+}
